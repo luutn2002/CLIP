ec7717ab14f1cdf14741821c9072540cb812a47f
luutn2002 <83963050+luutn2002@users.noreply.github.com>
2024-02-07 18:31:22 +0700

    Fix install bugs.

--- setup.py
+++ setup.py
@@ -16,5 +16,6 @@ setup(
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
         )
-    ]
+    ],
+    include_package_data=True
 )
